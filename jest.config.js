545d4443f3822eb02fa726626fcb8a7727dbfc0c
Alex Zaw <alex.zaw@azdevhub.com>
2024-02-11 12:13:51 -0500

    deleted:    jest.config.js 	modified:   package.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    moduleFileExtensions: ['js', 'json', 'vue'],
-    transform: {
-        '^.+\\.vue$': 'vue-jest',
-        '^.+\\.js$': 'babel-jest'
-    },
-    testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-    testURL: 'http://localhost/'
-}
