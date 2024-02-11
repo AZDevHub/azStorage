3b26e76c7e8c829109276a1a302999b26580f58b
Alex Zaw <alex.zaw@azdevhub.com>
2024-02-11 12:25:47 -0500

    modified:   src/routes/FileUpload.vue

--- src/routes/FileUpload.vue
+++ src/routes/FileUpload.vue
@@ -2,7 +2,15 @@
   <div class="app-container centered-box">
     <h1 class="about centered-box">Upload File</h1>
     <div>
-      <input class="hidden" id="upload" type="file" @change="uploadFile" ref="fileInput" required />
+      <input
+        class="hidden"
+        id="upload"
+        type="file"
+        @change="uploadFiles"
+        ref="fileInput"
+        multiple
+        required
+      />
       <button class="button" @click="selectFile" :disabled="isUploading">Upload</button>
     </div>
     <ShowPopup ref="popup" />
@@ -32,38 +40,32 @@ const showPopup = (message, type) => {
   popup.value.show(message, type)
 }
 
-const uploadFile = async (event) => {
+const uploadFiles = async (event) => {
   file.value = event.target.files[0]
   if (!file.value) {
     showPopup('Please select a file.', 'error')
     return
   }
   isUploading.value = true
-  setTimeout(() => {
-    isUploading.value = false
-  }, 6000)
   showPopup('Uploading...', 'loading')
-  try {
-    const formData = new FormData()
-    const uploadContainer = containerName.value || 'upload'
-    formData.append('fileInput', file.value)
-    const response = await apiClient.fileUpload(uploadContainer, formData)
-    console.log('File uploaded:', response.data)
-    showPopup('File uploaded successfully.', 'success')
-  } catch (error) {
-    console.error('Failed to upload file:', error)
-    showPopup('Failed to upload file. Please try again.', 'error')
-  } finally {
-    clearFile()
-    isUploading.value = false
+  for (const file of files) {
+    try {
+      const formData = new FormData();
+      const uploadContainer = containerName.value || 'upload';
+      formData.append('fileInput', file);
+      const response = await apiClient.fileUpload(uploadContainer, formData);
+      console.log('File uploaded:', response.data);
+    } catch (error) {
+      console.error('Failed to upload file:', error);
+      showPopup(`Failed to upload ${file.name}. Please try again.`, 'error');
+    }
   }
 }
-
-onMounted(() => {
-  watch(file, (newVal) => {
-    if (newVal) {
-      uploadFile()
-    }
-  })
-})
+//onMounted(() => {
+//  watch(file, (newVal) => {
+//    if (newVal) {
+//      uploadFile()
+//    }
+//  })
+//})
 </script>
